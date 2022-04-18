5974c7a94921412ba35bf98342f11e664b586367
Chris Kujawa <ckujawa@chriskujawadev.com>
2022-04-18 06:48:37 -0400

    start navbar creation

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,16 @@
 <template>
+  <the-navbar/>
   <div class="container">
-    <div>
-      <img alt="Vue logo" src="./assets/logo.png" />
-    </div>
     <router-view/>
   </div>
 </template>
 
 <script>
+import TheNavbar from '@/components/TheNavbar'
+
 export default {
-  name: 'App'
+  name: 'App',
+  components: { TheNavbar }
 }
 </script>
 
@@ -21,6 +22,5 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
